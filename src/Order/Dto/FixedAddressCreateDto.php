99c74351d2c7348e60c507eea785b2bbd152a370
Marek Gleska <marek.gleska@xebia.com>
2024-05-16 16:26:24 +0200

    Dto validator

--- src/Order/Dto/FixedAddressCreateDto.php
+++ src/Order/Dto/FixedAddressCreateDto.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace App\Order\Dto;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class FixedAddressCreateDto
 {
+    #[Assert\Length(min:1, max:100)]
+    #[Assert\Regex(pattern: '/^\S/', message: 'This value do not match regex pattern {{ pattern }}')]
     public string $externalId;
+
+    #[Assert\Length(min:1, max:250)]
+    #[Assert\Regex(pattern: '/^\S/', message: 'This value do not match regex pattern {{ pattern }}')]
     public string $nameCompanyOrPerson;
+
+    #[Assert\Length(min:1, max:250)]
+    #[Assert\Regex(pattern: '/^\S/', message: 'This value do not match regex pattern {{ pattern }}')]
     public string $address;
+
+    #[Assert\Length(min:1, max:250)]
+    #[Assert\Regex(pattern: '/^\S/', message: 'This value do not match regex pattern {{ pattern }}')]
     public string $city;
+
+    #[Assert\Length(min:1, max:50)]
+    #[Assert\Regex(pattern: '/^\S/', message: 'This value do not match regex pattern {{ pattern }}')]
     public string $zipCode;
 }

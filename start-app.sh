5137adfb840be9e247ad369c7ad3ca14ba3ce910
Marek Gleska <mgleska@gmail.com>
2024-09-27 15:16:51 +0200

    for Kubernetes

--- start-app.sh
+++ start-app.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-cd /app || exit 1
+set -ex
 
-composer install && \
-composer run-script apidoc && \
-docker/wait-for database:3306 && sleep 2 && \
-php bin/console doctrine:database:create --if-not-exists && \
-php bin/console doctrine:migrations:migrate --no-interaction && \
-php bin/console admin:init && \
+cd /app
+
+if [ "$1" == "dev" ]
+then
+  composer install
+  composer run-script apidoc
+fi
+
+docker/wait-for database:3306 -t 120
+sleep 2
+
+php bin/console doctrine:database:create --if-not-exists | tee /tmp/database-creation-result
+php bin/console doctrine:migrations:migrate --no-interaction
+grep -q 'Created database' /tmp/database-creation-result && php bin/console admin:init
+
 php -S 0.0.0.0:8000 -t public/
